41f4257bbfef6a0752152174eb631470af97323f
Ne02ptzero <louis@ne02ptzero.me>
2016-11-14 17:50:03 +0100

    Add(New codebase):
    Now based on dialog

---
+++ main.py
@@ -0,0 +1,91 @@
+#! /usr/bin/env python3
+################################### LICENSE ####################################
+#                            Copyright 2016 Morphux                            #
+#                                                                              #
+#        Licensed under the Apache License, Version 2.0 (the "License");       #
+#        you may not use this file except in compliance with the License.      #
+#                  You may obtain a copy of the License at                     #
+#                                                                              #
+#                 http://www.apache.org/licenses/LICENSE-2.0                   #
+#                                                                              #
+#      Unless required by applicable law or agreed to in writing, software     #
+#       distributed under the License is distributed on an "AS IS" BASIS,      #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#        See the License for the specific language governing permissions and   #
+#                       limitations under the License.                         #
+################################################################################
+
+##
+# main.py
+# Created: 14/11/2016
+# By: Louis Solofrizzo <louis@morphux.org>
+##
+
+import locale
+from pythondialog.dialog import *
+
+##
+# Global configuration
+##
+title = "Morphux Installer"
+version = "1.0"
+
+class   Main:
+
+##
+# Variables
+##
+    modules = {}
+    d = Dialog(dialog="dialog",autowidgetsize=True)
+    conf_lst = {}
+    screens = []
+
+##
+# Functions
+##
+    # Construct function
+    def     __init__(self):
+        self.load_screens()
+        self.get_screens_infos()
+        locale.setlocale(locale.LC_ALL, '')
+        self.d.set_background_title(title + ", version " + version)
+        self.main()
+
+
+    # Load differents modules in the path screens/
+    def     load_screens(self, path = "screens"):
+        res = {}
+        lst = os.listdir(path)
+        dir = []
+        for d in lst:
+            s = os.path.abspath(path) + os.sep + d
+            if os.path.isdir(s) and os.path.exists(s + os.sep + "__init__.py"):
+                dir.append(d)
+        for d in dir:
+            res[d] = __import__(path + "." + d + "." + d, fromlist = ["*"])
+            res[d] = getattr(res[d], d.title())
+        self.modules = res
+        self.get_screens_infos()
+
+    # Get infos on screens
+    def     get_screens_infos(self):
+        config = {}
+        for name, klass in self.modules.items():
+            print("Reading "+ name +" module ...  ", end="")
+            klass = klass()
+            config = klass.init(self.d, self.conf_lst)
+            self.screens.append(klass)
+            print("Done !")
+
+    def     main(self):
+        id = 0
+        t_id = 0
+        while 1:
+            t_id = self.screens[id].main()
+            if (t_id == -2 or (t_id == -1 and id == 0)):
+                return 0
+            elif (t_id != id):
+                id = t_id
+
+
+main = Main()
---
+++ screens/main_menu/main_menu.py
@@ -0,0 +1,90 @@
+################################### LICENSE ####################################
+#                            Copyright 2016 Morphux                            #
+#                                                                              #
+#        Licensed under the Apache License, Version 2.0 (the "License");       #
+#        you may not use this file except in compliance with the License.      #
+#                  You may obtain a copy of the License at                     #
+#                                                                              #
+#                 http://www.apache.org/licenses/LICENSE-2.0                   #
+#                                                                              #
+#      Unless required by applicable law or agreed to in writing, software     #
+#       distributed under the License is distributed on an "AS IS" BASIS,      #
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #
+#        See the License for the specific language governing permissions and   #
+#                       limitations under the License.                         #
+################################################################################
+
+##
+# main_menu.py
+# Created: 14/11/2016
+# By: Louis Solofrizzo <louis@morphux.org>
+##
+
+class   Main_Menu:
+
+##
+# Variables
+##
+    dlg = 0
+    conf_lst = {}
+    a_title = "\nWelcome to the Morphux Installer\n\
+For more information about the menu entries, See <Help> button";
+    choices = [
+        ("Install", "Launch system installation"),
+        ("Custom Install", "Launch install from a configuration file"),
+        ("Boot", "Just boot in the live CD"),
+        ("Launch Shell", "Execute /bin/sh"),
+        ("Options", "Open options screen"),
+        ("Exit", "Quit this program (will reboot)")
+    ]
+    choices_ref = {
+        "Install": {
+            "t_id": 1,
+            "help": "Launch the full, assisted, Morphux system installation"
+        },
+        "Custom Install": {
+            "t_id": 0,
+            "help": "Launch an automated install from a previous generated configuration file"
+        },
+        "Boot": {
+            "t_id": 0,
+            "help": "Enter the live-CD."
+        },
+        "Launch Shell": {
+            "t_id": 0,
+            "help": "Launch a basic shell. Advanced users only."
+        },
+        "Options": {
+            "t_id": 0,
+            "help": "Open the option screen"
+        },
+        "Exit": {
+            "t_id": 0,
+            "help": "Exit this program. This action will reboot the computer."
+        }
+    }
+
+##
+# Functions
+##
+    def     init(self, dialog, config_list):
+        self.dlg = dialog
+        self.conf_lst = config_list
+        self.config = {
+            "id": 0,
+            "name": "Main Menu"
+        }
+        return self.config
+
+    def     main(self):
+        code, tag = self.dlg.menu(self.a_title, title="Main Menu", choices = self.choices, help_button=True)
+        if (code == "ok" and tag == "Exit"):
+            return -2
+        elif (code == "help"):
+            self.dlg.msgbox(self.choices_ref[tag]["help"])
+            return 0
+        elif (code == "cancel"):
+            return -1
+        else:
+            return self.choices_ref[tag]["t_id"]
+
