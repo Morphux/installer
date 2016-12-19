aac3b3291138519a5cc0fd7cd1610a0966bc8424
Ne02ptzero <louis@ne02ptzero.me>
2016-12-19 18:35:58 +0100

    Add(Package pkg-config p2)

--- pkgs/pkgconfig_p2/pkgconfig_p2.py
+++ pkgs/pkgconfig_p2/pkgconfig_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# bzip_p2.py
-# Created: 16/12/2016
+# pkgconfig_p2.py
+# Created: 19/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Bzip2_P2:
+class   Pkgconfig_P2:
 
     conf_lst = {}
     e = False
@@ -33,38 +33,32 @@ class   Bzip2_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "bzip2", # Name of the package
-            "version": "1.0.6", # Version of the package
-            "size": 4.9, # Size of the installed package (MB)
-            "archive": "bzip2-1.0.6.tar.gz", # Archive name
-            "SBU": 0.1, # SBU (Compilation time)
+            "name": "pkgconfig", # Name of the package
+            "version": "0.29.1", # Version of the package
+            "size": 28, # Size of the installed package (MB)
+            "archive": "", # Archive name
+            "SBU": 0.3, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "pkgconfig", # Next package to install
+            "next": False, # Next package to install
+            "before": False,
+            "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/bzip2-1.0.6.tar.gz"
+                "https://install.morphux.org/packages/"
             ]
         }
         return self.config
 
-    def     before(self):
-        self.e(["patch", "-Np1", "-i", "../bzip2-1.0.6-install_docs-1.patch"])
-        self.e(["sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile"], shell=True)
-        return self.e(["sed -i 's@(PREFIX)/man@(PREFIX)/share/man@g' Makefile"])
-
     def     configure(self):
-        self.e(["make", "-f", "Makefile-libbz2_so"])
-        return self.e(["make", "clean"])
+        return self.e(["./configure",
+                "--prefix=/usr",
+                "--with-internal-glib",
+                "--disable-compile-warnings",
+                "--disable-host-tool",
+                "--docdir=/usr/share/doc/pkgconfig-0.29.1"
+        ])
 
     def     make(self):
         return self.e(["make", "-j", self.conf_lst["cpus"]])
 
     def     install(self):
-        return self.e(["make", "PREFIX=/usr" "install"])
-
-    def     after(self):
-        self.e(["cp", "-v", "bzip2-shared", "/bin/bzip2"])
-        self.e(["cp", "-av", "libbz2.so.*", "/lib"], shell=True)
-        self.e(["ln", "-sv", "../../lib/libbz2.so.1.0", "/usr/lib/libbz2.so"])
-        self.e(["rm", "-v", "/usr/bin/{bunzip2,bzcat,bzip2}"], shell=True)
-        self.e(["ln", "-sv", "bzip2", "/bin/bunzip2"])
-        return self.e(["ln", "-sv", "bzip2", "/bin/bzcat"])
+        return self.e(["make", "install"])
