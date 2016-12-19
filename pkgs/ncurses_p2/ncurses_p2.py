4f80323a1103d92d34fed6ac25bc4cf02b33cb29
Ne02ptzero <louis@ne02ptzero.me>
2016-12-19 18:49:35 +0100

    Add(Ncurses package)

--- pkgs/ncurses_p2/ncurses_p2.py
+++ pkgs/ncurses_p2/ncurses_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# pkgconfig_p2.py
+# ncurses_p2.py
 # Created: 19/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Pkgconfig_P2:
+class   Ncurses_P2:
 
     conf_lst = {}
     e = False
@@ -33,32 +33,54 @@ class   Pkgconfig_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "pkgconfig", # Name of the package
-            "version": "0.29.1", # Version of the package
-            "size": 28, # Size of the installed package (MB)
-            "archive": "", # Archive name
-            "SBU": 0.3, # SBU (Compilation time)
+            "name": "ncurses", # Name of the package
+            "version": "6.0", # Version of the package
+            "size": 38, # Size of the installed package (MB)
+            "archive": "ncurses-6.0.tar.gz", # Archive name
+            "SBU": 0.4, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "ncurses", # Next package to install
-            "before": False,
-            "after": False,
+            "next": False, # Next package to install
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/ncurses-6.0.tar.gz"
             ]
         }
         return self.config
 
+    def     before(self):
+        return self.e(["sed", "-i", "/LIBTOOL_INSTALL/d", "c++/Makefile.in"])
+
     def     configure(self):
         return self.e(["./configure",
                 "--prefix=/usr",
-                "--with-internal-glib",
-                "--disable-compile-warnings",
-                "--disable-host-tool",
-                "--docdir=/usr/share/doc/pkgconfig-0.29.1"
-        ])
+                "--mandir=/usr/share/man"
+                "--with-shared",
+                "--without-debug",
+                "--without-normal",
+                "--enable-pc-files",
+                "--enable-widec"
+            ])
 
     def     make(self):
         return self.e(["make", "-j", self.conf_lst["cpus"]])
 
     def     install(self):
         return self.e(["make", "install"])
+
+    def     after(self):
+        self.e(["mv", "-v", "/usr/lib/libncurses.so.6*", "/lib"], shell=True)
+        self.e(["ln -sfv ../../lib/$(readlink /usr/lib/libncursesw.so) /usr/lib/libncursesw.so"], shell=True)
+        self.e([
+            "for lib in ncurses form panel \
+rm -vf \
+echo 'INPUT(-l${lib}w)' > \
+ln -sfv ${lib}w.pc \
+done \
+menu ; do \
+/usr/lib/lib${lib}.so \
+/usr/lib/lib${lib}.so \
+/usr/lib/pkgconfig/${lib}.pc"
+        ], shell=True)
+        self.e(["rm", "-vf", "/usr/lib/libcursesw.so"])
+        self.e(["echo", "'INPUT(-lncursesw)'", ">", "/usr/lib/libcursesw.so"], shell=True)
+        self.e(["ln", "-sfv", "libncurses.so", "/usr/lib/libcurses.so"])
+
