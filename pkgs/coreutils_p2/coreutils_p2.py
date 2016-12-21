7a946240712641b2646d080eedb90bf177ae67db
Ne02ptzero <louis@ne02ptzero.me>
2016-12-21 14:35:46 +0100

    Add (Coreutils Package)

--- pkgs/coreutils_p2/coreutils_p2.py
+++ pkgs/coreutils_p2/coreutils_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# e2fsprogs_p2.py
+# coreutils_p2.py
 # Created: 21/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   E2fsprogs_P2:
+class   Coreutils_P2:
 
     conf_lst = {}
     e = False
@@ -33,50 +33,39 @@ class   E2fsprogs_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "e2fsprogs", # Name of the package
-            "version": "1.43.1", # Version of the package
-            "size": 54, # Size of the installed package (MB)
-            "archive": "", # Archive name
-            "SBU": 2.1, # SBU (Compilation time)
+            "name": "coreutils", # Name of the package
+            "version": "8.25", # Version of the package
+            "size": 168, # Size of the installed package (MB)
+            "archive": "coreutils-8.25.tar.xz", # Archive name
+            "SBU": 2.6, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "coreutils", # Next package to install
+            "next": False, # Next package to install
             "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/coreutils-8.25.tar.xz"
             ]
         }
         return self.config
 
     def     before(self):
-        self.e(["sed -i -e 's:\[\.-\]::' tests/filter.sed"], shell=True)
-        res = self.e(["mkdir", "build"])
-        os.chdir("build")
-        return res
+        self.e(["patch", "-Np1", "-i", "../coreutils-8.25-i18n-2.patch"])
 
     def     configure(self):
-        os.environ["LIBS"] = "-L/tools/lib"
-        os.environ["CFLAGS"] = "-I/tools/include"
-        os.environ["PKG_CONFIG_PATH"] = "/tools/lib/pkgconfig"
+        os.environ["FORCE_UNSAFE_CONFIGURE"] = "1"
         return self.e(["./configure",
                 "--prefix=/usr",
-                "--bindir=/bin",
-                "--with-root-prefix=''",
-                "--enable-elf-shlibs",
-                "--disable-libblkid",
-                "--disable-libuuid",
-                "--disable-uuidd",
-                "--disable-fsck"
-        ], shell=True)
+                "--enable-no-install-program=kill,uptime"
+        ])
 
     def     make(self):
         return self.e(["make", "-j", self.conf_lst["cpus"]])
 
     def     install(self):
         self.e(["make", "install"])
-        self.e(["make", "install-libs"])
-        self.e(["chmod -v u+w /usr/lib/{libcom_err,libe2p,libext2fs,libss}.a"], shell=True)
-        self.e(["gunzip -v /usr/share/info/libext2fs.info.gz"], shell=True)
-        self.e(["install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info"], shell=True)
-        self.e(["makeinfo -o doc/com_err.info ../lib/et/com_err.texinfo"], shell=True)
-        self.e(["install -v -m644 doc/com_err.info /usr/share/info"], shell=True)
-        return self.e(["install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info"], shell=True)
+        self.e(["mv -v /usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} /bin"], shell=True)
+        self.e(["mv -v /usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} /bin"], shell=True)
+        self.e(["mv -v /usr/bin/{rmdir,stty,sync,true,uname} /bin"], shell=True)
+        self.e(["mv -v /usr/bin/chroot /usr/sbin"], shell=True)
+        self.e(["mv -v /usr/share/man/man1/chroot.1 /usr/share/man/man8/chroot.8"], shell=True)
+        self.e(["sed -i s/\"1\"/\"8\"/1 /usr/share/man/man8/chroot.8"], shell=True)
+        self.e(["mv -v /usr/bin/{head,sleep,nice,test,[} /bin"], shell=True)
