87b2b718f68a165f403ce076026e6110754a43d6
Ne02ptzero <louis@ne02ptzero.me>
2016-12-22 22:06:38 +0100

    Add (Libpipeline Package)

--- pkgs/libpipeline_p2/libpipeline_p2.py
+++ pkgs/libpipeline_p2/libpipeline_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# kbd_p2.py
+# libpipeline_p2.py
 # Created: 22/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Kbd_P2:
+class   Libpipeline_P2:
 
     conf_lst = {}
     e = False
@@ -33,33 +33,28 @@ class   Kbd_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "kbd", # Name of the package
-            "version": "2.0.3", # Version of the package
-            "size": 29, # Size of the installed package (MB)
+            "name": "libpipeline", # Name of the package
+            "version": "1.4.1", # Version of the package
+            "size": 7.9, # Size of the installed package (MB)
             "archive": "", # Archive name
             "SBU": 0.1, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "libpipeline", # Next package to install
+            "next": False, # Next package to install
             "after": False,
+            "before": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
                 "https://install.morphux.org/packages/"
             ]
         }
         return self.config
 
-    def     before(self):
-        self.e(["patch", "-Np1", "-i", "../kbd-2.0.3-backspace-1.patch"])
-        self.e(["sed", "-i", "s/\(RESIZECONS_PROGS=\)yes/\1no/g", "configure"])
-        return self.e(["sed", "-i", "s/resizecons.8 //", "docs/man/man8/Makefile.in"])
-
     def     configure(self):
         return self.e(["PKG_CONFIG_PATH=/tools/lib/pkgconfig", "./configure",
-            "--prefix=/usr",
-            "--disable-vlock"
+            "--prefix=/usr"
         ])
 
     def     make(self):
         return self.e(["make", "-j", self.conf_lst["cpus"]])
 
     def     install(self):
-        return self.e(["make", "DOCDIR=/usr/share/doc/iproute2-4.7.0", "install"])
+        return self.e(["make", "install"])
