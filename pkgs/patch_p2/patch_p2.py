2120a5f1d9fd49caa8f730dc58858752e431cd09
Ne02ptzero <louis@ne02ptzero.me>
2016-12-22 22:10:22 +0100

    Add (Patch Package)

--- pkgs/patch_p2/patch_p2.py
+++ pkgs/patch_p2/patch_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# make_p2.py
+# patch_p2.py
 # Created: 22/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Make_P2:
+class   Patch_P2:
 
     conf_lst = {}
     e = False
@@ -33,17 +33,17 @@ class   Make_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "make", # Name of the package
-            "version": "4.2.1", # Version of the package
-            "size": 12.6, # Size of the installed package (MB)
-            "archive": "make-4.2.1.tar.bz2", # Archive name
-            "SBU": 0.6, # SBU (Compilation time)
+            "name": "patch", # Name of the package
+            "version": "2.7.5", # Version of the package
+            "size": 19.7, # Size of the installed package (MB)
+            "archive": "patch-2.7.5.tar.xz", # Archive name
+            "SBU": 0.2, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "patch", # Next package to install
+            "next": False, # Next package to install
             "before": False,
             "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/make-4.2.1.tar.bz2"
+                "https://install.morphux.org/packages/patch-2.7.5.tar.xz"
             ]
         }
         return self.config
