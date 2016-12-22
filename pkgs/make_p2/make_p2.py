305c71960607d56e481127adc6abc5e80d938ee3
Ne02ptzero <louis@ne02ptzero.me>
2016-12-22 22:08:43 +0100

    Add (Make Package)

--- pkgs/make_p2/make_p2.py
+++ pkgs/make_p2/make_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# libpipeline_p2.py
+# make_p2.py
 # Created: 22/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Libpipeline_P2:
+class   Make_P2:
 
     conf_lst = {}
     e = False
@@ -33,24 +33,24 @@ class   Libpipeline_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "libpipeline", # Name of the package
-            "version": "1.4.1", # Version of the package
-            "size": 7.9, # Size of the installed package (MB)
-            "archive": "", # Archive name
-            "SBU": 0.1, # SBU (Compilation time)
+            "name": "make", # Name of the package
+            "version": "4.2.1", # Version of the package
+            "size": 12.6, # Size of the installed package (MB)
+            "archive": "make-4.2.1.tar.bz2", # Archive name
+            "SBU": 0.6, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "make", # Next package to install
-            "after": False,
+            "next": False, # Next package to install
             "before": False,
+            "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/make-4.2.1.tar.bz2"
             ]
         }
         return self.config
 
     def     configure(self):
-        return self.e(["PKG_CONFIG_PATH=/tools/lib/pkgconfig", "./configure",
-            "--prefix=/usr"
+        return self.e(["./configure",
+                "--prefix=/usr",
         ])
 
     def     make(self):
