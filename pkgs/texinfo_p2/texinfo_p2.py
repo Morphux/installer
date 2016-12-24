ee8e94198effd6de09225b376f8ca86c2ef88636
Ne02ptzero <louis@ne02ptzero.me>
2016-12-24 22:51:36 +0100

    Add (Texinfo Package)

--- pkgs/texinfo_p2/texinfo_p2.py
+++ pkgs/texinfo_p2/texinfo_p2.py
@@ -15,14 +15,14 @@
 ################################################################################
 
 ##
-# tar_p2.py
+# texinfo_p2.py
 # Created: 24/12/2016
 # By: Louis Solofrizzo <louis@morphux.org>
 ##
 
 import      os
 
-class   Tar_P2:
+class   Texinfo_P2:
 
     conf_lst = {}
     e = False
@@ -33,26 +33,25 @@ class   Tar_P2:
         self.e = ex
         self.root_dir = root_dir
         self.config = {
-            "name": "tar", # Name of the package
-            "version": "1.29", # Version of the package
-            "size": 40, # Size of the installed package (MB)
-            "archive": "tar-1.29.tar.xz", # Archive name
-            "SBU": 2.2, # SBU (Compilation time)
+            "name": "texinfo", # Name of the package
+            "version": "6.1", # Version of the package
+            "size": 108, # Size of the installed package (MB)
+            "archive": "texinfo-6.1.tar.xz", # Archive name
+            "SBU": 0.5, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
-            "next": "texinfo", # Next package to install
+            "next": False, # Next package to install
             "before": False,
             "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/tar-1.29.tar.xz"
+                "https://install.morphux.org/packages/texinfo-6.1.tar.xz"
             ]
         }
         return self.config
 
     def     configure(self):
-        os.environ["FORCE_UNSAFE_CONFIGURE"] = "1"
         return self.e(["./configure",
                 "--prefix=/usr",
-                "--bindir=/bin"
+                "--disable-static"
         ])
 
     def     make(self):
