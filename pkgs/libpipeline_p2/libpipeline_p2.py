1170dc514f18721474c39c1d494d19654abda8d1
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(libpipeline_p2 package): Add archive url

--- pkgs/libpipeline_p2/libpipeline_p2.py
+++ pkgs/libpipeline_p2/libpipeline_p2.py
@@ -36,14 +36,14 @@ class   Libpipeline_P2:
             "name": "libpipeline", # Name of the package
             "version": "1.4.1", # Version of the package
             "size": 7.9, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "libpipeline-1.4.1.tar.gz", # Archive name
             "SBU": 0.1, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": "make", # Next package to install
             "after": False,
             "before": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/libpipeline-1.4.1.tar.gz"
             ]
         }
         return self.config
