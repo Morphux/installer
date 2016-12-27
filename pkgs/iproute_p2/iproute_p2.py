ca8b770a725bef618e3d3a48c906435b4b3780a9
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(iproute_p2 package): Add archive url

--- pkgs/iproute_p2/iproute_p2.py
+++ pkgs/iproute_p2/iproute_p2.py
@@ -36,14 +36,14 @@ class   Iproute_P2:
             "name": "iproute", # Name of the package
             "version": "4.7.0", # Version of the package
             "size": 11, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "iproute2-4.7.0.tar.xz", # Archive name
             "SBU": 0.2, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": False, # Next package to install
             "after": "kbd",
             "configure": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/iproute2-4.7.0.tar.xz"
             ]
         }
         return self.config
