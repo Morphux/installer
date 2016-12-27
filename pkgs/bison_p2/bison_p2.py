a78f4723c1427ee60c5e843a6cdb96100bbe3e70
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(bison_p2 package): Add archive url

--- pkgs/bison_p2/bison_p2.py
+++ pkgs/bison_p2/bison_p2.py
@@ -36,14 +36,14 @@ class   Bison_P2:
             "name": "bison", # Name of the package
             "version": "3.0.4", # Version of the package
             "size": 32, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "bison-3.0.4.tar.xz", # Archive name
             "SBU": 0.3, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": "flex", # Next package to install
             "before": False,
             "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/bison-3.0.4.tar.xz"
             ]
         }
         return self.config
