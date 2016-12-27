4869297a5f61bb8a4c047f9626051ce3b714ca9d
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(flex_p2 package): Add archive url

--- pkgs/flex_p2/flex_p2.py
+++ pkgs/flex_p2/flex_p2.py
@@ -36,13 +36,13 @@ class   Flex_P2:
             "name": "flex", # Name of the package
             "version": "2.6.1", # Version of the package
             "size": 31, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "flex-2.6.1.tar.xz", # Archive name
             "SBU": 0.4, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": "grep", # Next package to install
             "before": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/flex-2.6.1.tar.xz"
             ]
         }
         return self.config
