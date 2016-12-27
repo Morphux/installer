2ff6020ae23aa5172e69a5e1676a8f136c1985a3
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(kmod_p2 package): Add archive url

--- pkgs/kmod_p2/kmod_p2.py
+++ pkgs/kmod_p2/kmod_p2.py
@@ -36,14 +36,14 @@ class   Kmod_P2:
             "name": "kmod", # Name of the package
             "version": "23", # Version of the package
             "size": 10.3, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "kmod-23.tar.xz", # Archive name
             "SBU": 0.1, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": "gettext", # Next package to install
             "after": False,
             "before": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/kmod-23.tar.xz"
             ]
         }
         return self.config
