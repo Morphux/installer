36e387c8311ba1387ad0bf59c770cc56c3fa93d6
Ne02ptzero <louis@ne02ptzero.me>
2016-12-27 11:54:23 +0100

    Fix(inetutils_p2 package): Add archive url

--- pkgs/inetutils_p2/inetutils_p2.py
+++ pkgs/inetutils_p2/inetutils_p2.py
@@ -36,14 +36,14 @@ class   Inetutils_P2:
             "name": "inetutils", # Name of the package
             "version": "1.9.4", # Version of the package
             "size": 27, # Size of the installed package (MB)
-            "archive": "", # Archive name
+            "archive": "inetutils-1.9.4.tar.xz", # Archive name
             "SBU": 0.4, # SBU (Compilation time)
             "tmp_install": False, # Is this package part of the temporary install
             "next": "perl", # Next package to install
             "before": False,
             "after": False,
             "urls": [ # Url to download the package. The first one must be morphux servers
-                "https://install.morphux.org/packages/"
+                "https://install.morphux.org/packages/inetutils-1.9.4.tar.xz"
             ]
         }
         return self.config
