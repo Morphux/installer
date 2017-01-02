bcd67d51cf7b330b1b32e09df7033dc7c03f9c4e
Ne02ptzero <louis@ne02ptzero.me>
2017-01-02 15:00:52 +0100

    Fix(Toolchain): Add GCC respec for new linker

--- pkgs/toolchain_p2/toolchain_p2.py
+++ pkgs/toolchain_p2/toolchain_p2.py
@@ -55,4 +55,5 @@ class   Toolchain_P2:
         self.e(["mv", "-v", "/tools/bin/ld", "/tools/bin/ld-old"])
         self.e(["mv -v /tools/$(uname -m)-pc-linux-gnu/bin/{ld,ld-old}"], shell=True, ignore=True)
         self.e(["mv", "-v", "/tools/bin/{ld-new,ld}"], shell=True)
+        self.e(["gcc -dumpspecs | sed -e 's@/tools@@g' -e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' -e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' > `dirname $(gcc --print-libgcc-file-name)`/specs"], shell=True)
         return self.e(["ln", "-sv", "/tools/bin/ld", "/tools/$(uname -m)-pc-linux-gnu/bin/ld"], shell=True)
