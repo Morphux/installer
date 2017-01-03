08136b82cba3e6879d5122ed1701a3b7e456f429
Ne02ptzero <louis@ne02ptzero.me>
2017-01-03 18:00:45 +0100

    Fix(Acl): Missing single-quote

--- pkgs/acl_p2/acl_p2.py
+++ pkgs/acl_p2/acl_p2.py
@@ -49,7 +49,7 @@ class   Acl_P2:
     def     before(self):
         self.e(["sed", "-i", "-e", "s|/@pkg_name@|&-@pkg_version@|", "include/builddefs.in"])
         self.e(["sed -i 's:| sed.*::g' test/{sbits-restore,cp,misc}.test"], shell=True)
-        return self.e(["sed -i -e '/TABS-1;/a if (x > (TABS-1)) x = (TABS-1);libacl/__acl_to_any_text.c"], shell=True)
+        return self.e(["sed -i -e '/TABS-1;/a if (x > (TABS-1)) x = (TABS-1);' libacl/__acl_to_any_text.c"], shell=True)
 
     def     configure(self):
         return self.e(["./configure",
